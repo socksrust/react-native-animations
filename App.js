420a29f09cdbfe5ca0fb38400919ff2ed9c7376d
jaburcodes <guilhermejabur@outlook.com>
2018-04-10 00:10:41 -0300

    Add components and react-navigator

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//@flow
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
-import AnimationsList from './src/screens/AnimationsList';
+import { StackNavigator } from 'react-navigation';
 
-type Props = {};
-export default class App extends Component<Props> {
+//Main animations Views
+import Animated from './src/screens/AnimatedList';
+import Animatable from './src/screens/AnimatableList';
+import Lottie from './src/screens/LottieList';
+
+//Animated Views
+
+//Animatable Views
+
+//Lottie Views
+import SendEmailLottie from './src/Lottie/SendEmail'
+import RocketLottie from './src/Lottie/Rocket'
+import WorldLottie from './src/Lottie/World'
+
+const RootStack = StackNavigator(
+  {
+    Lottie: {
+      screen: Lottie,
+    },
+    Animated: {
+      screen: Animated,
+    },
+    Animatable: {
+      screen: Animatable,
+    },
+
+    //Lottie Views
+    SendEmailLottie: {
+      screen: SendEmailLottie,
+    },
+    WorldLottie: {
+      screen: WorldLottie,
+    },
+    RocketLottie: {
+      screen: RocketLottie,
+    },
+
+  },
+  {
+    initialRouteName: 'Lottie',
+  }
+);
+
+class App extends Component<*> {
   render() {
-    return (
-      <AnimationsList />
-    );
+    return <RootStack />
   }
 }
+
+export default App;
